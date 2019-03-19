be91e83dd5060ff1dedd68c309cf3282a98982c5
sagar097 <spawar097@gmail.com>
2019-03-19 16:42:12 +0530

    ggg

---
+++ react-graphql-apollo/public/precache-manifest.440547952cd3001e26713cd9404c0756.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58267fab8108437e3ae",
+    "url": "/static/css/main.c06c9c23.chunk.css"
+  },
+  {
+    "revision": "b58267fab8108437e3ae",
+    "url": "/static/js/main.ef95d60a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b4096c249edef8993de9",
+    "url": "/static/js/2.bd9c8ad3.chunk.js"
+  },
+  {
+    "revision": "3c490f267b6862c008b4387e1d812d08",
+    "url": "/index.html"
+  }
+];
